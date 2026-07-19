326d70df28ef95432ea6cbcb6f92f6a17743fd64
0xPoe <poe.liu@pm.me>
2026-07-19 10:39:36 +0200

    lr: clean up
    Signed-off-by: 0xPoe <poe.liu@pm.me>

--- lr/src/questions/q_0125_valid_palindrome.rs
+++ lr/src/questions/q_0125_valid_palindrome.rs
@@ -1,96 +1,28 @@
 pub fn is_palindrome(s: String) -> bool {
-    if !s.is_ascii() {
-        return false;
-    }
-
-    let s = s.to_lowercase();
-    let low_case_s = s.trim();
-    let mut temp = String::new();
-
-    for c in low_case_s.chars() {
-        if c.is_ascii_lowercase() || c.is_ascii_digit() {
-            temp.push(c);
-        }
-    }
-
-    if temp.is_empty() {
-        return true;
-    }
-
-    let rev_temp = temp.chars().rev().collect::<String>();
-    temp == rev_temp
-}
-
-pub fn is_palindrome_two_pointers(s: String) -> bool {
-    if !s.is_ascii() {
-        return false;
-    }
-
-    let s = s.chars().collect::<Vec<_>>();
+    let chars: Vec<char> = s.to_lowercase().chars().collect();
     let (mut i, mut j) = (0, s.len() - 1);
-
     while i < j {
-        if !s[i].is_alphanumeric() {
+        if !chars[i].is_ascii_alphanumeric() {
             i += 1;
             continue;
         }
-        if !s[j].is_alphanumeric() {
+        if !chars[j].is_ascii_alphanumeric() {
             j -= 1;
             continue;
         }
-        if s[i].to_lowercase().to_string() != s[j].to_ascii_lowercase().to_string() {
+        if chars[i] != chars[j] {
             return false;
         }
+
         i += 1;
         j -= 1;
     }
-
-    true
-}
-
-pub fn is_palindrome_queue(s: String) -> bool {
-    use std::collections::VecDeque;
-    let mut deque = VecDeque::new();
-
-    for c in s.chars() {
-        if c.is_alphanumeric() {
-            deque.push_back(c.to_ascii_lowercase());
-        }
-    }
-
-    while deque.len() > 1 {
-        if deque.pop_front() != deque.pop_back() {
-            return false;
-        }
-    }
-
     true
 }
 
 #[test]
 fn test_is_palindrome() {
-    assert!(!is_palindrome("0P".to_string()));
-    assert!(is_palindrome(" ".to_string()));
     assert!(is_palindrome("A man, a plan, a canal: Panama".to_string()));
-    assert!(!is_palindrome("race a car".to_string()));
-}
-
-#[test]
-fn test_is_palindrome_two_pointers() {
-    assert!(!is_palindrome_two_pointers("0P".to_string()));
-    assert!(is_palindrome_two_pointers(" ".to_string()));
-    assert!(is_palindrome_two_pointers(
-        "A man, a plan, a canal: Panama".to_string()
-    ));
-    assert!(!is_palindrome_two_pointers("race a car".to_string()));
-}
-
-#[test]
-fn test_is_palindrome_quque() {
-    assert!(!is_palindrome_queue("0P".to_string()));
-    assert!(is_palindrome_queue(" ".to_string()));
-    assert!(is_palindrome_queue(
-        "A man, a plan, a canal: Panama".to_string()
-    ));
-    assert!(!is_palindrome_queue("race a car".to_string()));
+    assert!(!is_palindrome("0P".to_string()));
+    assert!(!is_palindrome("00PA".to_string()));
 }
